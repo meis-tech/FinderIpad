2cda9cb6433dbad567cd7323a8fa54451872ed59
Alex Flohr <AlexFlohr@lawn-128-61-42-108.lawn.gatech.edu>
2015-09-26 19:02:31 -0400

    Initial Commit

---
+++ BeaconFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconFinder
+//
+//  Created by Alex Flohr on 9/26/15.
+//  Copyright (c) 2015 Alex Flohr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
